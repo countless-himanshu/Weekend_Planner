49d82624636a261374a2680b2e05425de48f9acc
countless-himanshu <himanshu12120202@gmail.com>
2025-09-14 21:16:58 +0530

    comments about main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,3 +1,5 @@
+//This `index.jsx` is the entry point of the React application, rendering the main `App` component inside the HTML element with ID `root` while applying global styles from `index.css` and wrapping the app in `React.StrictMode` to help identify potential problems during development.
+
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
